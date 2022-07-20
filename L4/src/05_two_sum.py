70d7995dad134bf74d3e6d83d49bd5def5e250c7
Alexander Stepanov <alexandr.stepanov@koyfin.com>
2022-07-20 21:09:11 +0200

    L4: two sum

--- L4/src/05_two_sum.py
+++ L4/src/05_two_sum.py
@@ -11,4 +11,7 @@ from typing import List
 def twoSum(nums: List[int], target: int) -> List[int]:
     print(nums, target)
 
-twoSum([1, 2, 3], 4)
+twoSum([1, 2, 3], 4)        # [0, 2]
+twoSum([2, 7, 11, 15], 9)   # [0, 1]
+twoSum([3, 2, 4], 6)        # [1, 2]
+twoSum([3, 3], 6)           # [0, 1]
